1727423b4728bf86307f31cc284820226e4fbf36
Tavilo Breno <breno_wk2@hotmail.com>
2024-03-07 15:30:05 -0300

    Criação do modelo de usuário

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username', // Novo campo adicionado
     ];
+    
 
     /**
      * The attributes that should be hidden for serialization.
